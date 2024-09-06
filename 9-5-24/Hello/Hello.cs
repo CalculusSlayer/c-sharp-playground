e60e3ef4078d26995dac49d50c761e6ac415464a
Nayeel Imtiaz <nayeelimtiaz11@gmail.com>
2024-09-05 19:26:33 -0700

    created simple hello world program + instructions to compile/run

---
+++ 9-5-24/Hello/Hello.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Instruction to set up project:
+// 1) 1) `dotnet new console --output {Directory name}` if no project setup yet
+
+// Instructions to compile and run:
+// 1) `dotnet build` (optional)
+// 2) `dotnet run` in project directory
+
+using System;
+
+class Program {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World! It is 9-5-24.");
+    }
+}
