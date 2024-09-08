bf67fd5f5e58ad3ca9552b823d35c6a0d6439881
Nayeel Imtiaz <nayeelimtiaz11@gmail.com>
2024-09-08 00:42:38 -0700

    played with c# arrays

---
+++ 9-7-24/Arrays/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args)
+    {
+        Array myArr = Array.CreateInstance(typeof(int), 2, 3, 4);
+        Console.WriteLine("The Array has {0} dimension(s) and a total of {1} elements.", myArr.Rank, myArr.Length);
+        Console.WriteLine("The Array has {0} dimension(s) and a total of {1} elements.", myArr.Rank, myArr.Length);
+    }
+}
